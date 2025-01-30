0f8228c0dec70be86fad13da798690881c431922
WJijon223 <williamjijon223@gmail.com>
2025-01-30 13:00:04 -0500

    created Car class, additional brand attribute, and constructor

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,5 +1,10 @@
 package org.example;
 
-public class Car {
+public class Car extends Vehicle {
+    private String brand;
 
+    Car(int numOfWheels, String color, float engineSize, String fuelType, String brand) {
+        super(numOfWheels, color, engineSize, fuelType);
+        this.brand = brand;
+    }
 }
